5e26382b39b43560cd8da60b95fb99ae97e0abbb
Mukul Sharma <mukuls@fractalanalytics.com>
2014-09-02 18:49:53 +0530

    Adding ThmymeLeaf Support

--- src/main/java/com/list/app/config/WebAppConfig.java
+++ src/main/java/com/list/app/config/WebAppConfig.java
@@ -1,11 +1,18 @@
 package com.list.app.config;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+import org.thymeleaf.templateresolver.TemplateResolver;
 
 /**
  * Author : Mukul.Sharma
@@ -16,12 +23,42 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @EnableWebMvc
 public class WebAppConfig extends WebMvcConfigurerAdapter {
 
-	@Bean
-	public InternalResourceViewResolver setupViewResolver() {
-		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-		viewResolver.setPrefix("/WEB-INF/views/");
-		viewResolver.setSuffix(".jsp");
+	/*
+	 * View Resolver -> Template Engine -> Template Resolver
+	 */
 
+	@Bean
+	public ServletContextTemplateResolver templateResolver() {
+		ServletContextTemplateResolver resolver = new ServletContextTemplateResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".html");
+		// NB, selecting HTML5 as the template mode.
+		resolver.setTemplateMode("HTML5");
+		resolver.setCacheable(false);
+		// resolver.setOrder(2);
+		return resolver;
+
+	}
+
+	@Bean
+	public SpringTemplateEngine templateEngine() {
+		SpringTemplateEngine engine = new SpringTemplateEngine();
+
+		Set<TemplateResolver> templateResolvers = new HashSet<TemplateResolver>();
+		templateResolvers.add(templateResolver());
+
+		engine.setTemplateResolvers(templateResolvers);
+
+		return engine;
+	}
+
+	@Bean
+	public ViewResolver viewResolver() {
+		ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+		viewResolver.setTemplateEngine(templateEngine());
+		viewResolver.setOrder(1);
+		viewResolver.setViewNames(new String[] { "*" });
+		viewResolver.setCache(false);
 		return viewResolver;
 	}
 
